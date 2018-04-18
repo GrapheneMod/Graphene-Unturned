d4ea3b4fbffa818b450e351ecfe5e5a35cf37d9c
Viskalix <Viskalix@users.noreply.github.com>
2018-04-18 15:32:14 +0200

    Initial push
    * Set everything up
    * Created Loader class as an Unturned Module

---
+++ Graphene-Unturned/Loader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using SDG.Framework.Modules;
+
+namespace Graphene.Unturned
+{
+    internal class Loader : IModuleNexus
+    {
+        #region IModuleNexus Functions
+        public void initialize()
+        {
+        }
+
+        public void shutdown()
+        {
+        }
+        #endregion
+    }
+}
